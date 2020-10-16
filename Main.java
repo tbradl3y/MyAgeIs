ded416f43989633aa0e2c39050222785a4a0be5f
tbradley <tommy.bradley@patelhs.org>
2020-10-16 15:42:51 +0000

    prints out sentence back to you

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ class Main {
   int currentYear = 2020;
   System.out.println("What's your first name?");
   String name = scan.next();
+  name = name + scan.nextLine();
   // asking you to fill out your information
   System.out.println(" Whats your age");
   int age = scan.nextInt();
@@ -19,7 +20,7 @@ class Main {
    System.out.println ("your graduating class is "+ currentYear);
    //prints out your graduating class
   }else if (age >= 18){ 
- //if you're younger than 15 no yearbook quote
+ //if you're younger than 18 no yearbook quote
   System.out.println("What's your yearbook quote?");
   String quote = scan.next();
   quote = quote + scan.nextLine();
